7e0f5e1035561f347fd8d23c58ce7e3fe35f9aef
Adam Maas <adamjm1220@gmail.com>
2025-03-07 15:00:23 -0500

    Initial COmmit

--- modules/api_gateway_task_1/key.tf
+++ modules/api_gateway_task_1/key.tf
@@ -34,7 +34,7 @@ resource "aws_api_gateway_usage_plan" "pi_side_smart_home_api_gateway_task_1_usa
 
 #Link API Key to the Usage Plan
 resource "aws_api_gateway_usage_plan_key" "pi_side_smart_home_api_gateway_task_1_key_usage" {
-    usage_plan_id = aws_apigateway_usage_plan.pi_side_smart_home_api_gateway_task_1_usage_plan.id
-    key_id = aws_apigateway_api_key.pi_side_smart_home_task_1_api_key.id
+    usage_plan_id = aws_api_gateway_usage_plan.pi_side_smart_home_api_gateway_task_1_usage_plan.id
+    key_id = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.id
     key_type = "API_KEY"
 }
--- modules/api_gateway_task_1/outputs.tf
+++ modules/api_gateway_task_1/outputs.tf
@@ -1,14 +1,13 @@
-# output "api_key_for_task_1"{
-#     value = aws_apigatewayv2_api_key.pi_side_smart_homeapi_key.key
-# }
+output "api_key_for_task_1_sensitive"{
+    value = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.value
+    sensitive = true
+}
 
+output "api_key_for_task_1_value_not_sensitive"{
+    value = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.value
+    sensitive = false
+}
 
-
-# # resource "aws_apigatewayv2_api_key" "pi_side_smart_homeapi_key"{
-# #     name = "pi_side_smart_homeapi_key"
-# #     description = "API Key for the task 1 API Gateway"
-
-# #     tags = {
-# #         Name = "pi_side_smart_homeapi_key"
-# #     }
-# # }
+output "api_key_for_task_1_value_no_config"{
+    value = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.value
+}
