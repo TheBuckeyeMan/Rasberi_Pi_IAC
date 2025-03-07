c4da1d6ddb162ce30a88663e536f6669bc7b170e
Adam Maas <adamjm1220@gmail.com>
2025-03-07 15:02:36 -0500

    Initial COmmit

--- modules/api_gateway_task_1/outputs.tf
+++ modules/api_gateway_task_1/outputs.tf
@@ -2,12 +2,3 @@ output "api_key_for_task_1_sensitive"{
     value = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.value
     sensitive = true
 }
-
-output "api_key_for_task_1_value_not_sensitive"{
-    value = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.value
-    sensitive = false
-}
-
-output "api_key_for_task_1_value_no_config"{
-    value = aws_api_gateway_api_key.pi_side_smart_home_task_1_api_key.value
-}
