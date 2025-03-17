321f7b7411c566cd0724c0327ce12ae849770092
Adam Maas <adamjm1220@gmail.com>
2025-03-17 18:41:45 -0400

    Added IoT Fleet Provisioning_Template

--- modules/iot_fleet_template/main.tf
+++ modules/iot_fleet_template/main.tf
@@ -11,15 +11,14 @@ resource "aws_iot_provisioning_template" "pi_side_smart_home_iot_cert_tmp"{
 
     template_body = jsonencode({
         Parameters = {
-        SerialNumber = {
-            Type = "String"
-        }
+            SerialNumber = { Type = "String"}
+            CertificateId = { Type = "String" }
         },
         Resources = {
             certificate = {
                 Type = "AWS::IoT::Certificate", #Makes the Certificate
                 Properties = {
-                    CertificateId = { Ref = "AWS::IoT::Certificate::Id" }
+                    CertificateId = { Ref = "CertificateId" }
                     Status = "ACTIVE"
                 }
             },
