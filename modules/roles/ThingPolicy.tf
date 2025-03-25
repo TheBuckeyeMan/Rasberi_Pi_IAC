48508178102649c4a534283f56c3d4d398dd24f6
Adam Maas <adamjm1220@gmail.com>
2025-03-25 19:19:16 -0400

    Added IoT Fleet Provisioning_Template

--- modules/roles/ThingPolicy.tf
+++ modules/roles/ThingPolicy.tf
@@ -1,8 +1,7 @@
 
 
-resource "aws_iam_policy" "pi_side_smart_home_thing_policy"{
+resource "aws_iot_policy" "pi_side_smart_home_thing_policy"{
     name = "pi_side_smart_home_thing_policy"
-    description = "Policy for NEW THING SET UP"
 
     policy = jsonencode({
     "Version" : "2012-10-17",
